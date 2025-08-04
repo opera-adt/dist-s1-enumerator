b3ce8ea4a0b4a6c13ce10a9fc84003502f837896
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2025-08-04 16:53:10 -0700

    fix input fxn

--- src/dist_s1_enumerator/dist_enum_inputs.py
+++ src/dist_s1_enumerator/dist_enum_inputs.py
@@ -6,7 +6,7 @@ from dist_s1_enumerator.asf import get_rtc_s1_ts_metadata_from_mgrs_tiles
 from dist_s1_enumerator.dist_enum import enumerate_dist_s1_products
 
 
-def update_dist_s1_input_dict(data_dict: dict) -> dict:
+def update_dist_s1_workflow_dict(data_dict: dict) -> dict:
     out = {}
     out.update(
         {
@@ -17,9 +17,8 @@ def update_dist_s1_input_dict(data_dict: dict) -> dict:
     )
     out_formatted = {
         'mgrs_tile_id': out['mgrs_tile_id'],
-        'acq_date': out['acq_date_for_mgrs_pass'],
+        'post_acq_date': out['acq_date_for_mgrs_pass'],
         'track_number': out['track_number'],
-        'product_id': out['product_id'],
     }
     return out_formatted
 
@@ -30,6 +29,10 @@ def enumerate_dist_s1_workflow_inputs(
     start_acq_dt: datetime | str | None = None,
     stop_acq_dt: datetime | str | None = None,
     lookback_strategy: str = 'multi_window',
+    max_pre_imgs_per_burst: int | list[int] | tuple[int, ...] = 10,
+    min_pre_imgs_per_burst: int = 1,
+    delta_lookback_days: int | list[int] | tuple[int, ...] = 0,
+    delta_window_days: int = 365,
 ) -> list[dict]:
     if isinstance(mgrs_tile_ids, str):
         mgrs_tile_ids = [mgrs_tile_ids]
@@ -46,16 +49,27 @@ def enumerate_dist_s1_workflow_inputs(
         start_acq_dt,
         stop_acq_dt,
     )
+    df_products = enumerate_dist_s1_products(
+        df_ts,
+        mgrs_tile_ids,
+        lookback_strategy=lookback_strategy,
+        max_pre_imgs_per_burst=max_pre_imgs_per_burst,
+        min_pre_imgs_per_burst=min_pre_imgs_per_burst,
+        delta_lookback_days=delta_lookback_days,
+        delta_window_days=delta_window_days,
+    )
+
+    df_post = df_products[df_products['input_category'] == 'post'].reset_index(drop=True)
+    df_s1_workflow_inputs = df_post.groupby(['product_id']).first().reset_index(drop=True)
+
     if start_acq_dt is not None:
         start_ind = df_ts.acq_dt >= pd.Timestamp(start_acq_dt, tz='UTC')
-        df_ts = df_ts[start_ind].reset_index(drop=True)
+        df_s1_workflow_inputs = df_s1_workflow_inputs[start_ind].reset_index(drop=True)
     if stop_acq_dt is not None:
         stop_ind = df_ts.acq_dt <= pd.Timestamp(stop_acq_dt, tz='UTC')
-        df_ts = df_ts[stop_ind].reset_index(drop=True)
+        df_s1_workflow_inputs = df_s1_workflow_inputs[stop_ind].reset_index(drop=True)
 
-    df_products = enumerate_dist_s1_products(df_ts, mgrs_tile_ids, lookback_strategy=lookback_strategy)
+    df_s1_workflow_input_data = df_s1_workflow_inputs.to_dict('records')
 
-    df_dist_prod_data = df_products.to_dict('records')
-
-    all_inputs = list(map(update_dist_s1_input_dict, df_dist_prod_data))
-    return all_inputs
+    df_s1_workflow_input_data_formatted = list(map(update_dist_s1_workflow_dict, df_s1_workflow_input_data))
+    return df_s1_workflow_input_data_formatted
